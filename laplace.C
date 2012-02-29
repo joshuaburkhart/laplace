a9d9ef1ea678f29fcc000fec294a9acd32279e09
Joshua Burkhart <burkhart.joshua@gmail.com>
2012-02-28 23:47:51 -0800

    working in parallel... try 3d, visualization

--- laplace.C
+++ laplace.C
@@ -29,8 +29,8 @@ int np;			// number of processors
 int main(int argc, char *argv[]) {
   OptionList opt(argc,argv);
 
-  nx = opt.getInt("nx",200);
-  ny = opt.getInt("ny",200);
+  nx = opt.getInt("nx",10);
+  ny = opt.getInt("ny",10);
   eps = opt.getDouble("eps",1.0);
   np = opt.getInt("np",1);
 
@@ -54,17 +54,68 @@ int main(int argc, char *argv[]) {
   //initiate top row to HI_TEMP
   for(int col_num=0;col_num<nx;col_num++){
     *(*h+col_num)=HI_TEMP;
+    *(*g+col_num)=HI_TEMP;
   }
   //initiate bottom rows to LO_TEMP
   for(int row_num=1;row_num<ny;row_num++){
     for(int col_num=0;col_num<nx;col_num++){
       *(*(h+row_num)+col_num)=LO_TEMP;
+      *(*(g+row_num)+col_num)=LO_TEMP;
     }
   }
-  
-  output(h,nx,ny);
 
-  omp_set_num_threads(4);
+  bool converged=false;
+
+  while(converged==false){
+    converged=true;
+    #pragma omp parallel for
+    for(int row_num=1;row_num<(ny-1);row_num++){//leaving top and bottom rows
+      for(int col_num=1;col_num<(nx-1);col_num++){
+	int r = row_num;
+	int c = col_num;
+	double left=*(*(h+r)+(c-1));
+        double right=*(*(h+r)+(c+1));
+	double up=*(*(h+(r-1))+c);
+	double down=*(*(h+(r+1))+c);
+	double twin=*(*(h+r)+c);
+        #pragma critical
+	{
+          printf("hello from %i\n",omp_get_thread_num());
+	  *(*(g+r)+c)=0.25*(left+right+up+down);
+	}
+        double self=*(*(g+r)+c);
+        if((self*self - twin*twin) > eps){
+	  converged=false;
+	}
+	/*
+	printf("r is %i\n",r);
+	printf("c is %i\n",c);
+	printf("left is %f\n",left);
+        printf("right is %f\n",right);
+        printf("up is %f\n",up);
+        printf("down is %f\n",down);
+	printf("twin is %f\n",twin);
+	*/
+      }
+    }
+  printf("--------------before--------------------------------------\n");
+  printf("g\n");
+  output(g,nx,ny);
+  printf("h\n");
+  output(h,nx,ny);
+    double** temp=g;
+    g=h;
+    h=temp;
+  printf("--------------after-------------------------------------\n");
+  printf("g\n");
+  output(g,nx,ny);
+  printf("h\n");
+  output(h,nx,ny);
+    
+  }
+
+
+  //omp_set_num_threads(4);
   #pragma omp parallel
   { 
     #pragma omp critical
