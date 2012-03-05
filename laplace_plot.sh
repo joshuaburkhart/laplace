ffc302102229c765b7650e31ab96162d6e105859
Joshua Burkhart <burkhart.joshua@gmail.com>
2012-03-04 16:38:17 -0800

    adding default cmd args

--- laplace_plot.sh
+++ laplace_plot.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 #Usage:
 #./laplace_plot.sh 200 200 .05 9 1 1 1
-
-./bin/laplace -nx $1 -ny $2 -eps $3 -fx $4 -fy $5 -fg $6 -bh $7 > "./out/output.csv"
+NX=${1:-200}
+NY=${2:-200}
+EPS=${3:-.05}
+FX=${4:-9}
+FY=${5:-1}
+FG=${6:-1}
+BH=${7:-1}
+./bin/laplace -nx $NX -ny $NY -eps $EPS -fx $FX -fy $FY -fg $FG -bh $BH > "./out/output.csv"
 R --no-save << EOT
 A = scan(file="./out/output.csv",sep=",")
-A = matrix(A,ncol=($1*$2),nrow=3)
+A = matrix(A,ncol=($NX*$NY),nrow=3)
 A = t(A)
 par(pty="s")
 palette(rainbow(384,end=.75))
