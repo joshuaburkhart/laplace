a71fa8059dd03d076d4b0953d9398610a5862a48
Joshua Burkhart <burkhart.joshua@gmail.com>
2012-03-04 17:55:52 -0800

    sequential fin code executing

--- laplace.C
+++ laplace.C
@@ -20,13 +20,12 @@ void output(double** array, int nx, int ny);
 int main(int argc, char *argv[])
 {
   //defaults
-  int nx=10;			// number of points, x dimension
-  int ny=10;			// number of points, y dimension
-  double eps=1.0;		// epsilon (stopping condition)
-  int fx=9;			// fin width, x dimension
-  int fy=1;			// fin width, y dimension
-  int fg=1;			// gap between adjacent fins
-  int bh=1;			// base height
+  int nx    =330;		// number of points, x dimension
+  int ny    =70;		// number of points, y dimension
+  double eps=0.05;		// epsilon (stopping condition)
+  int fx    =30;			// fin width (x dimension)
+  int fy    =55;		// fin length (y dimension)
+  int fg    =30;			// gap between adjacent fins
 
   //parsing args
   for(int i=0;i<argc;i++){
@@ -48,10 +47,27 @@ int main(int argc, char *argv[])
     else if(0==strcmp(argv[i],"-fg")){
       fg=atoi(argv[i+1]);
     }
-    else if(0==strcmp(argv[i],"-bh")){
-      bh=atoi(argv[i+1]);
-    }
   }
+
+  int fx_total   = fx+fg;
+  int base_height= ny-fy;
+  int num_fins   = nx/fx_total;
+
+  if(num_fins % 2 != 0){
+    num_fins++; //place another fin on the base ... if it will fit
+  }
+
+  /*
+  printf("nx is %i\n",nx);
+  printf("ny is %i\n",ny);
+  printf("eps is %f\n",eps);
+  printf("fx is %i\n",fx);
+  printf("fy is %i\n",fy);
+  printf("fg is %i\n",fg);
+  printf("fx_total is %i\n",fx_total);
+  printf("num_fins is %i\n",num_fins);
+  printf("base_height is %i\n",base_height);
+  */
 
   double **g,**h; //we will read from h and operate on g
   g = (double **) malloc(ny*sizeof(double*));
@@ -60,10 +76,10 @@ int main(int argc, char *argv[])
     *(g+row_num)= (double*) malloc(nx*sizeof(double));
     *(h+row_num)= (double*) malloc(nx*sizeof(double));
   }
-  //initiate top row to HI_TEMP
-  for(int col_num=0;col_num<nx;col_num++){
-    *(*h+col_num)=HI_TEMP;
-    *(*g+col_num)=HI_TEMP;
+  //initiate base to HI_TEMP
+    for(int col_num=0;col_num<nx;col_num++){
+      *(*h+col_num)=HI_TEMP;
+      *(*g+col_num)=HI_TEMP;
   }
   //initiate bottom rows to LO_TEMP
   for(int row_num=1;row_num<ny;row_num++){
@@ -77,24 +93,48 @@ int main(int argc, char *argv[])
 
   while(converged==false){
     converged=true;
-    #pragma omp parallel for
-    for(int row_num=1;row_num<(ny-1);row_num++){//leaving top and bottom rows
-      for(int col_num=1;col_num<(nx-1);col_num++){
-	int r = row_num;
-	int c = col_num;
-	double left=*(*(h+r)+(c-1));
-        double right=*(*(h+r)+(c+1));
-	double up=*(*(h+(r-1))+c);
-	double down=*(*(h+(r+1))+c);
-	double twin=*(*(h+r)+c);
-        #pragma critical
-	{
-	  *(*(g+r)+c)=0.25*(left+right+up+down);
-	}
-        double self=*(*(g+r)+c);
-        if((self*self - twin*twin) > eps){
-	  converged=false;
-	}
+    //calculating base
+    //#pragma omp parallel for
+      for(int row_num=1;row_num<base_height;row_num++){
+        for(int col_num=1;col_num<(nx-1);col_num++){
+	  int r = row_num;
+	  int c = col_num;
+	  double left=*(*(h+r)+(c-1));
+          double right=*(*(h+r)+(c+1));
+	  double up=*(*(h+(r-1))+c);
+	  double down=*(*(h+(r+1))+c);
+	  double twin=*(*(h+r)+c);
+          //#pragma critical
+	  {
+	    *(*(g+r)+c)=0.25*(left+right+up+down);
+            double self=*(*(g+r)+c);
+            if((self*self - twin*twin) > eps){
+	      converged=false;
+	    }
+	  }
+        }
+      }
+    //calculating fins
+    for(int fin=0;fin<num_fins;fin++){
+      //#pragma omp parallel for
+      for(int row_num=base_height;row_num<(ny-1);row_num++){
+        for(int col_num=fx_total*fin+1;col_num<(fx_total*fin+fx-1);col_num++){
+	  int r = row_num;
+	  int c = col_num;
+	  double left=*(*(h+r)+(c-1));
+          double right=*(*(h+r)+(c+1));
+	  double up=*(*(h+(r-1))+c);
+	  double down=*(*(h+(r+1))+c);
+	  double twin=*(*(h+r)+c);
+          //#pragma critical
+	  {
+	    *(*(g+r)+c)=0.25*(left+right+up+down);
+            double self=*(*(g+r)+c);
+            if((self*self - twin*twin) > eps){
+	      converged=false;
+	    }
+	  }
+        }
       }
     }
     double** temp=g;
--- laplace_plot.sh
+++ laplace_plot.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #Usage:
 #./laplace_plot.sh 200 200 .05 9 1 1 1
-NX=${1:-200}
-NY=${2:-200}
-EPS=${3:-.05}
-FX=${4:-9}
-FY=${5:-1}
-FG=${6:-1}
-BH=${7:-1}
-./bin/laplace -nx $NX -ny $NY -eps $EPS -fx $FX -fy $FY -fg $FG -bh $BH > "./out/output.csv"
+NX=${1:-330}
+NY=${2:-70}
+EPS=${3:-0.05}
+FX=${4:-30}
+FY=${5:-55}
+FG=${6:-30}
+./bin/laplace -nx $NX -ny $NY -eps $EPS -fx $FX -fy $FY -fg $FG > "./out/output.csv"
 R --no-save << EOT
 A = scan(file="./out/output.csv",sep=",")
 A = matrix(A,ncol=($NX*$NY),nrow=3)
